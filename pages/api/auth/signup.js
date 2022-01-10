8b87a8ae950a0be6b0b3c394f58a002dc81633f2
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-01-10 08:54:55 +0200

    Sending Signup Requests From The Frontend

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,24 +2,27 @@ import { hashPassword } from "../../../lib/auth";
 import { connectToDatabase } from "../../../lib/db";
 
 const handler = async (req, res) => {
+  if (req.method !== "POST") {
+    return;
+  }
   const data = req.body;
 
-  const { email, password } = data;
+  const { email, password } = JSON.parse(data);
 
-  if (
-    !email ||
-    !email.includes("@") ||
-    !password ||
-    password.trim().length < 6
-  ) {
-    res.status(422).json({ message: "Invalid input" });
+  if (!email || !email.includes("@")) {
+    res.status(422).json({ message: "Invalid email" });
+    return;
+  }
+
+  if (!password || password.trim().length < 6) {
+    res.status(422).json({ message: "Invalid password" });
     return;
   }
 
   const client = await connectToDatabase();
   const db = client.db();
 
-  const hashedPassword = hashPassword(password);
+  const hashedPassword = await hashPassword(password);
 
   const result = db
     .collection("users")
